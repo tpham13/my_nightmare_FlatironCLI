dfb5b7e7d813f29aac1d532269e1729b562e455d
tpham13 <tpham0123@gmail.com>
2020-06-19 16:46:08 -0700

    list the coffee. need to list in new line

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,53 +1,71 @@
 # require_relative "../lib.my_nightmare_CLI.rb"
 require 'nokogiri'
 require 'open-uri'
-
-class CoffeeBlends
-    attr_accessor :url, :coffee
-    attr_reader :name
-    @@all = []
-
-    def initialize(name)
-        @name = name
-        @coffee = []
-        @@all << self 
-    end 
-
-    def self.all
-        @@all
-    end 
-
-    def self.check_for_coffee_blends(coffee_blends)
-        self.all.detect {|x| x.name == coffee_blends}
-    end 
-end 
+require_relative 'coffee_blends'
+require_relative 'coffee'
+require 'pry'
 
 # class Scraper
-
     def get_page(url)
-        page = Nokogiri::HTML(open(url))            #::HTML parsing the pageq
+        page = Nokogiri::HTML(open(url))            
         # page
     end 
 
-    def get_coffee_blends
-        page = get_page("https://www.philzcoffee.com/shop")
-        # test = page.css("div.span4.cat-row a")              
-        page.css("div.cat-title span").each do |coffee_blend|  #css is a selector that help get the page
-            coffee_blend_name = coffee_blend.text
-            new_coffee_blend = CoffeeBlends.new(coffee_blend_name)
-            # puts new_coffee_blend.name
+    def get_coffee_blends       #Questions: 1. How do I get the page?  2. What values do I need from the HTML? 3. From the structure of the HTML, where are the values that I need? 4. What is the expected syntax for the .css method?
+        page = get_page("https://www.philzcoffee.com/shop")         
+        page.css("div.span4.cat-row").each do |coffee_blend|  
+           
+            if CoffeeBlends.all.length <3
+            coffee_blend_name = coffee_blend.css("span").children[0]
+            coffee_blend_url = coffee_blend.css("a").attribute("href").value 
+            new_coffee_blend = CoffeeBlends.new(coffee_blend_name)          #need to figure out how to filter only the first three blends (dark, medium, light)
+            new_coffee_blend.url = "https://www.philzcoffee.com#{coffee_blend_url}"
+            end          
+        end  
     end 
    
-    def get_coffee_blends
+    def get_coffee
+        get_coffee_blends
+        CoffeeBlends.all.each do |coffee_blend|
+        end     
+            page = get_page(coffee_blend.url)
+            binding.pry
+            # page.css("div.facets-item-cell-grid-details span").each do |product|
+            # page.css("div.facets-item-cell-grid-details").text #.each do |product| 
+            # page.css("div.facets-item-cell-grid-details span").children[0].text
+            page.css("div.facets-item-cell-grid-details a span").children.text
+           
+            
+                #     new_coffee = CoffeeBlends.new(coffee_blend.css("h6").next)
+                #     new_coffee.url = "https://www.philzcoffee.com" + coffee_blend.css("a").attribute("href").value
+                #     new_coffee.coffee_blends = coffee_blend
+                #     category.coffee << new_coffee
+                # end 
+                
+            # end 
+         
     end 
- 
-    # def get_coffee_description
-    # end 
-
-    # def get_coffee_price
-    # end 
-
-
-
 
 # end 
+# puts get_coffee_blends.name
+    # def get_coffee_info
+    # end 
+    
+   
+
+    
+
+# end 
+#  Test code for get_coffee_blends: 
+    # get_coffee_blends
+    # CoffeeBlends.all.each do |coffee_blend|
+    #     puts coffee_blend.name
+    # end  
+# end
+ 
+    # def get_coffee
+    # get_coffee
+    #     # get_coffee_blends
+    #     CoffeeBlends.all.each do |coffee_blend|
+    #     puts coffee_blend.name
+    # end 
