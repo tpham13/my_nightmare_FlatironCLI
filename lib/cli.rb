05a0c4fd2e42332931ff7a974f9dfda4771bcf79
tpham13 <tpham0123@gmail.com>
2020-06-20 22:17:49 -0700

    coffeeCLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,15 @@
 #CLI Controller 
 class Coffee::class 
-    # def initialize
-        
-    # end
+    
+    def scrape_for_coffee
+    end
+
+    def display_coffee_blends      #call for class coffee_categories
+    end
+
+    def display_coffee           #call for class coffee_clends
+    end 
+
+    def description_for_one_coffee
+    end 
 end
---
+++ lib/coffee_blends.rb
@@ -0,0 +1,20 @@
+class CoffeeBlends
+    attr_accessor :url, :coffee
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @coffee = []
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def self.check_for_coffee_blends(coffee_blends)
+        self.all.detect {|x| x.name == coffee_blends}
+    end 
+end 
+
