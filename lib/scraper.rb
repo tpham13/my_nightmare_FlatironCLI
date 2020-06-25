26a4c3888a9c82bc1e8b2abff4e4fb7e7f8dc9d5
tpham13 <tpham0123@gmail.com>
2020-06-24 23:53:00 -0700

    coffee_product return nil

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,7 @@
-# require_relative "../lib.my_nightmare_CLI.rb"
-require 'nokogiri'
-require 'open-uri'
-require_relative 'coffee_blends'
-require_relative 'coffee'
-require 'pry'
+require_relative "../config/environment.rb"
 
 class Scraper
+
     def get_page(url)
         page = Nokogiri::HTML(open(url))            
         
@@ -16,12 +12,13 @@ class Scraper
         page.css("div.span4.cat-row").each do |coffee_blend|  
            
             if CoffeeBlends.all.length <3
-            coffee_blend_name = coffee_blend.css("span").children[0]
+            coffee_blend_name = coffee_blend.css("span").children[0].text
             coffee_blend_url = coffee_blend.css("a").attribute("href").value 
             new_coffee_blend = CoffeeBlends.new(coffee_blend_name)          #need to figure out how to filter only the first three blends (dark, medium, light)
             new_coffee_blend.url = "https://www.philzcoffee.com#{coffee_blend_url}"
-            end          
-        end  
+            end         
+        end 
+        # binding.pry 
     end 
    
     def get_coffee
@@ -34,14 +31,16 @@ class Scraper
             page.css("div.facets-item-cell-grid-details").each do |product|
 
             if coffee_blend.coffee.length < 3
-                coffee_product = page.css("div.facets-item-cell-grid-details a span").children[i]            
-                #puts coffee_product
+                coffee_product = page.css("div.facets-item-cell-grid-details a span").children[i].text  #added .text         
+                #puts coffee_product  
+                binding.pry 
                 new_coffee_product = Coffee.new(coffee_product)
                 new_coffee_product.url = "https://www.philzcoffee.com" + product.css("a").attribute("href").value
                 new_coffee_product.blend = coffee_blend
                 coffee_blend.coffee << new_coffee_product
                 i += 1
             end
+            # binding.pry 
             end 
         end 
          end 
@@ -51,9 +50,11 @@ class Scraper
     def get_coffee_description(coffee)
        
             page = get_page(coffee.url)
-            coffee.description = page.css("div.product-details-description").children[0]
-    end 
-     
+           
+            coffee.description = page.css("div.product-details-description").children[0] #.text   #added .text here to see if the cli get_description method will work
+            
+     end 
+    #  binding.pry
     def get_this_coffee_description(coffee_name) 
         coffee = Coffee.find_by_name(coffee_name)
         if !coffee.check_for_coffee_description
