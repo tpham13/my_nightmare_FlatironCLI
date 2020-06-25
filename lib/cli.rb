960e258b77f3e67e07c3845d17a0610a2b6e7cde
tpham13 <tpham0123@gmail.com>
2020-06-25 00:00:09 -0700

    #display_coffee works

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,7 +3,8 @@
 require_relative "../config/environment.rb"
 
 class Coffee_cli
-    attr_accessor :scraper, :blend, :name
+    attr_accessor :scraper, :blend
+    attr_reader :name
     
     def call
         puts "Welcome to the Coffee CLI App. Please wait while the app is loading..."
@@ -13,6 +14,7 @@ class Coffee_cli
         # titleize
         display_coffee_blends
         display_coffee
+        display_description
     end    
     
     def initialize
@@ -22,16 +24,11 @@ class Coffee_cli
     end 
 
     def scrape_for_coffee
-        # puts "Welcome to the Coffee CLI App. Please wait while the app is loading..."
         puts ""
         @scraper = Scraper.new
         @scraper.get_coffee
+        # @scraper.get_coffee_blends 
     end
-
-    def titleize(word)
-        title = word.split.map(&:capitalize).join(' ')
-        title
-    end 
 
     def display_coffee_blends      #call for  coffee_blends class
         puts "Here are all of Philz Coffee available blends:"
@@ -41,37 +38,57 @@ class Coffee_cli
             puts "  #{index+1}. #{blend.name}"
             
         end  
-        puts ""
-        puts "Please enter the name of the coffee blend that you're interested in:" #need an if statement for when the user enter the blend to execute the next code based on user's decision
-        puts ""
     end
+ 
 
-    # def select_blend
-    # end 
+    def display_coffee     #call for class coffee
+        puts ""
+        puts "Please enter the name of the coffee blend that you're interested in:" 
+        puts "For example, enter 'Darker Blends' if you want to see the coffees available for that blend"
+        input = gets.chomp
+        blend = input.split.map(&:capitalize).join(' ')
 
-    def display_coffee(blend)     #call for class coffee
-        blend_name = titleize(blend)
-        blend_coffees = Coffee.all_coffees_in(blend_name)
+        # blend_name = titleize(blend)
+        # puts blend_name
+
+        blend_coffees = Coffee.all_coffees_in(blend)
+        
+
         if blend_coffees.length > 0 
-            puts "Here are all of our #{blend}: "
+       
+            puts "Here are all of our products for #{blend}: "
             blend_coffees.each_with_index do |coffee, index|
-                puts "#{index+1}. #{titleize(coffee.name)}"
+                puts "#{index+1}. #{coffee.name}"
             end 
+         
         else
             puts "Sorry that doesn't appear to be a valid blend..."
             display_coffee_blends
+        
         end 
     end 
 
+    def display_description
+        puts ""
+        puts "What type of coffee would you like to learn about?"
+            input_2 = gets
+        description = input.split.map(&:capitalize).join(' ')
+        coffee_description = Coffee.check_for_coffee_description(input_2)
+        
+        if coffee_description.length > 0 
+
+            puts "#{coffee_description}"
+            cofffee_description.each_with_index do |description, index|
+                puts "#{index+1}. #{description.name}"
+            end 
+        end 
+    end 
+
+end 
      
-end
-
-# a = Coffee_cli.new 
-# b = a.display_coffee_blends
-# c = a.display_coffee("Lighter Blends")
 
 
-Coffee_cli.new.call
+
 
 
 
