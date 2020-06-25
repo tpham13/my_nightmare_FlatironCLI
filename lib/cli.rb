736e5bac410e43ce426c64a8b0d0d4d0dd4ba0ea
tpham13 <tpham0123@gmail.com>
2020-06-25 16:01:13 -0700

    all methods in cli work

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,20 +7,20 @@ class Coffee_cli
     attr_reader :name
     
     def call
-        puts "Welcome to the Coffee CLI App. Please wait while the app is loading..."
-        # scrape_for_coffee
-        # initialize
-        # scrape_for_coffee
-        # titleize
+        puts ""
+        puts "=========================================================================================="
+        puts "Welcome to the Coffee CLI App."
+        puts "This app will allow you to learn all about the various coffees that Philz Coffee provides."
+        puts "Please wait while the app is loading..."
+        puts ""
+        
         display_coffee_blends
         display_coffee
         display_description
     end    
     
-    def initialize
-        
-        self.scrape_for_coffee
-        # @call = []
+    def initialize  
+        self.scrape_for_coffee   
     end 
 
     def scrape_for_coffee
@@ -30,31 +30,30 @@ class Coffee_cli
         # @scraper.get_coffee_blends 
     end
 
-    def display_coffee_blends      #call for  coffee_blends class
+    def display_coffee_blends      
         puts "Here are all of Philz Coffee available blends:"
         puts ""
     
-        CoffeeBlends.all.each_with_index do |blend, index|      #this will place an interger and a dot in front of each blend
+        CoffeeBlends.all.each_with_index do |blend, index|      
             puts "  #{index+1}. #{blend.name}"
             
         end  
     end
  
 
-    def display_coffee     #call for class coffee
+    def display_coffee   
+        
         puts ""
-        puts "Please enter the name of the coffee blend that you're interested in:" 
-        puts "For example, enter 'Darker Blends' if you want to see the coffees available for that blend"
-        input = gets.chomp
+        puts "Please type the blend that you're interested in and press 'Enter' if you want to see the coffees available for that blend."
+        puts ""
+        # puts "If you are done using the Coffee app, type Exit and press Enter."
+
+        input = gets
         blend = input.split.map(&:capitalize).join(' ')
-
-        # blend_name = titleize(blend)
-        # puts blend_name
-
+        puts ""
         blend_coffees = Coffee.all_coffees_in(blend)
         
-
-        if blend_coffees.length > 0 
+        if blend_coffees.length > 0         #if there are coffee under this blend then:
        
             puts "Here are all of our products for #{blend}: "
             blend_coffees.each_with_index do |coffee, index|
@@ -64,49 +63,61 @@ class Coffee_cli
         else
             puts "Sorry that doesn't appear to be a valid blend..."
             display_coffee_blends
-        
+            display_coffee
+
         end 
     end 
 
     def display_coffee_info(coffee_name)
         coffee = Coffee.find_by_name(coffee_name)
             puts " "
-            puts "Here are the details for our #{coffee.name} coffee"
+            puts "Here are the details for our #{coffee.name} coffee:"
             puts coffee.description
-
     end 
 
     def display_description
-        puts ""
-        puts "What type of coffee would you like to learn about?"
-            input = gets
-            description = input.split.map(&:capitalize).join(' ')
-            # coffee = Coffee.all.find{|coffee| coffee.name == description}     #match user type to string
+        input = nil
+        until input == 'Back' || input == 'back'
+            puts ""
+            puts "What type of coffee would you like to learn about?"
+            puts "Type the name of the coffee to get its description or type 'Back' to go to the previous page"
+            puts ""
+
+            input = gets.chomp
+            nocaps = ["the", "of"]
+            description = input.split(" ").map { |word| nocaps.include?(word) ? word : word.capitalize }.join(" ")
             coffee = Coffee.find_by_name(description)
-            #puts coffee_name.description
+
             if coffee != nil 
-                # info_for_one_coffee(coffee.name)
                 @scraper.get_this_coffee_description(coffee.name)
                 display_coffee_info(coffee.name)
-            else 
-                puts " "
-                puts "Bummer! It looks like that isn't a valid coffee name."
-                puts " "
-            end     
-    
-        #$coffee_description = Coffee.check_for_coffee_description(coffee_name)
-       
-        # if coffee.length > 0 
-
-            # puts "#{coffee}"
-            # cofffee.each_with_index do |description, index|
-            #     puts "#{index+1}. #{description.name}"
-            # end 
+                until input == 'Exit' || input == 'exit'
+                    puts ""
+                    puts "Would you like to look at more coffees or would you like to exit now?"
+                    puts "If you want to look at more coffees, enter 'Home'"
+                    puts "If you want to exit, enter 'Exit'"
+                    input = gets.chomp
+                    if input == "Home" || input == "home"
+                        call
+                    elsif input == "Exit" || input == "exit"
+                        puts "goodbye"
+                        abort
+                    else
+                        puts "please try again"
+                    end 
+            end
             
+            elsif input == "Back" || input == "back"
+                call
+
+            else 
+                puts "Please try again"
+           
+            end
+        end    
+
     end 
         
-    
-
 end 
      
 
