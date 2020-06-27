2a645f42990878d8439d25793a037b8e49b47a65
tpham13 <tpham0123@gmail.com>
2020-06-26 22:51:27 -0700

    refactored methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,12 +7,10 @@ class Coffee_cli
     attr_reader :name
     
     def call
-        puts ""
-        puts "=========================================================================================="
+
+        puts "\n ========================================================================================== \n\n"
         puts "Welcome to the Coffee CLI App."
-        puts "This app will allow you to learn all about the various coffees that Philz Coffee provides."
-        puts "Please wait while the app is loading..."
-        puts ""
+        puts "This app will allow you to learn all about the various coffees that Philz Coffee provides. \n\n"
         
         display_blends
         display_coffees
@@ -26,37 +24,34 @@ class Coffee_cli
     
     def titleize(word)
         nocaps = ["the", "of"]
-        title = word.split(" ").map { |word| nocaps.include?(word) ? word : word.capitalize }.join(" ")
+        title = word.split(" ").map { |word| nocaps.include?(word) ? 
+                word : word.capitalize }.join(" ")
         title
     end
     
     def display_blends      
-        puts "Here are all of Philz Coffee available blends:"
-        puts ""
-    
+        puts "Here are all of Philz Coffee available blends: \n\n"
         CoffeeBlends.all.each_with_index do |blend, index|      
             puts "  #{index+1}. #{blend.name}"
         end  
     end
  
-    def user_choice_for_blend
-        
-        puts ""
-        puts "Please type the blend that you're interested in and press 'Enter' if you want to see the coffees available for that blend."
-        puts ""
+    def user_choice
         input = gets.chomp
-        blend = titleize(input)
-            
+        choice = titleize(input)        
     end 
 
-    def display_coffees   #refactor: 2 methods--> 1. get user choice, 2. display coffee using the choice
+    def display_coffees  
+
+        puts "\nPlease type the blend that you're interested in and press 'Enter' if you want to see the coffees available for that blend. \n\n"
         
-        choice = user_choice_for_blend
-        blend_coffees = Coffee.all_coffees_in(choice)
+        input = user_choice
+        puts ""
+        blend_coffees = Coffee.all_coffees_in(input)
         
         if blend_coffees.length > 0         
        
-            puts "Here are all of our products for #{choice}: "
+            puts "Here are all of our coffees for #{input}: "
             blend_coffees.each_with_index do |coffee, index|
                 puts "#{index+1}. #{coffee.name}"
             end 
@@ -65,45 +60,38 @@ class Coffee_cli
             puts "Sorry that doesn't appear to be a valid blend..."
             display_blends
             display_coffees
-
         end 
     end 
 
-    def display_coffee_info(coffee_name)
-        coffee = Coffee.find_by_name(coffee_name)
-            puts " "
-            puts "Here are the details for our #{coffee.name} coffee:"
-            puts coffee.description
-    end 
-
-    def display_description             #refactor: 1. user choice  2.
+    def display_description             
         input = nil
         until input == 'Back' || input == 'back'
-            puts ""
-            puts "What type of coffee would you like to learn about?"
-            puts "Type the name of the coffee to get its description or type 'Back' to go to the previous page"
-            puts ""
+            puts "\nWhat type of coffee would you like to learn about?"
+            puts "Type the name of the coffee to get its description or type 'Back' to go to the previous page. \n\n"
 
-            input = gets.chomp 
-            description = titleize(input)
-            coffee = Coffee.find_by_name(description)
+            input = user_choice
+            coffee = Coffee.find_by_name(input)
 
             if coffee != nil 
                 @scraper.get_this_coffee_description(coffee.name)
-                display_coffee_info(coffee.name)
+                puts "\nHere are the details for our #{coffee.name} coffee:"
+                puts coffee.description
+
                 until input == 'Exit' || input == 'exit'
-                    puts ""
-                    puts "Would you like to look at more coffees or would you like to exit now?"
-                    puts "If you want to look at more coffees, enter 'Home'"
-                    puts "If you want to exit, enter 'Exit'"
-                    input = gets.chomp
+                    puts "\nWould you like to look at more coffees or would you like to exit now?"
+                    puts "If you want to look at more coffees, enter 'Home'."
+                    puts "If you want to exit, enter 'Exit'. \n\n"
+            
+                    input = user_choice
+                    
                     if input == "Home" || input == "home"
                         call
+                    
                     elsif input == "Exit" || input == "exit"
-                        puts "goodbye"
+                        puts "\nThank you for using our  Coffee CLI App. Goodbye! \n\n" 
                         exit 
                     else
-                        puts "please try again"
+                        puts "Please try again"
                     end 
             end
             
